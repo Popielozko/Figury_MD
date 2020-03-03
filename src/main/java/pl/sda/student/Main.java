6606a10b6dab5c42c1bf80d3692865a259a9eae2
popielozko <maciejd69@gmail.com>
2020-03-03 19:14:32 +0100

    koniec

--- src/main/java/pl/sda/student/Main.java
+++ src/main/java/pl/sda/student/Main.java
@@ -6,11 +6,11 @@ public class Main {
         Triangle triangle = new Triangle(12, 13);
         Rectangle rectangle = new Rectangle(23, 12);
 
-        System.out.println("Pole trojkata to" + triangle.getArea(triangle.getSideA(), triangle.getSideB()));
-        System.out.println("Obwód trojkata to" + triangle.getPerimeter(triangle.getSideA(), triangle.getSideB()));
+        System.out.println("Pole trojkata to " + triangle.getArea(triangle.getSideA(), triangle.getSideB()));
+        System.out.println("Obwód trojkata to " + triangle.getPerimeter(triangle.getSideA(), triangle.getSideB()));
 
-        System.out.println("Pole prostokąta to" + rectangle.getArea(rectangle.getSideA(), rectangle.getSideB()));
-        System.out.println("Obwod prostokata to" + rectangle.getPerimeter(rectangle.getSideA(), rectangle.getSideB()));
+        System.out.println("Pole prostokąta to " + rectangle.getArea(rectangle.getSideA(), rectangle.getSideB()));
+        System.out.println("Obwod prostokata to " + rectangle.getPerimeter(rectangle.getSideA(), rectangle.getSideB()));
     }
 
 
